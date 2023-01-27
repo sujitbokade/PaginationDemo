5bf0c9b4422826d0fe2523f28477ddf2c504479e
jagveer singh <jagveersingh.55555@gmail.com>
2023-01-27 12:37:26 +0530

    Initial Commit

---
+++ PaginationProject/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  PaginationProject
+//
+//  Created by Macbook on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var limit = 20
+    var totalCountries = 0
+    var index = 0
+    
+    var displayCountries:[String] = Array()
+    @IBOutlet weak var myTable: UITableView!
+    
+    var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalCountries = country_list.count
+        
+        while index < limit {
+            displayCountries.append(country_list[index])
+            index = index + 1
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayCountries.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row + 1).\(displayCountries[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == displayCountries.count - 1 {
+            let index = displayCountries.count - 1
+            if index+20 > country_list.count - index {
+                limit = country_list.count - index
+            } else {
+                limit = index + 20
+            }
+        }
+        
+        while index < limit {
+            displayCountries.append(country_list[index])
+            index = index + 1
+        }
+        
+        self.perform(#selector(loadTable), with: nil, afterDelay: 0.6)
+    }
+    @objc func loadTable() {
+        self.myTable.reloadData()
+    }
+}
